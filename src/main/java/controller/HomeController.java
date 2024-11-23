d25a8513402d9b002317cffb80adcbfe3b476fc4
RTRIT <petertringuyen03@gmail.com>
2024-11-23 21:33:19 +0700

    HelloControlller

--- src/main/java/controller/HomeController.java
+++ src/main/java/controller/HomeController.java
@@ -1,15 +1,15 @@
-package Controller;
+package controller;
 
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping("/test")
 public class HomeController {
 
-    @RequestMapping("/testing")
-    public String Test(){
-        return "index";
+    @RequestMapping("/hello")
+    public String getPage(){
+        return "welcome";
     }
 }
